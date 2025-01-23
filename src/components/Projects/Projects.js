e1e0fc99818193d02ed214f225ea56ecf59a4cb8
rolwyn <rolwyn.quadras051@gmail.com>
2025-01-23 05:28:57 -0500

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import kpit from "../../Assets/Projects/kpit.png";
-import CitiusTech from "../../Assets/Projects/CitiusTech_Logo.png";
+// import CitiusTech from "../../Assets/Projects/CitiusTech_Logo.png";
 import peapod from "../../Assets/Projects/peapod.png";
 import aggrepay from "../../Assets/Projects/aggrepay.png";
 import getsethome from "../../Assets/Projects/getsethome.png";
